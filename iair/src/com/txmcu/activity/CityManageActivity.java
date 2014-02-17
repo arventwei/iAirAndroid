281cde602fe4e16ffc0da5feda5d62e6a12001b4
arventwei <arventwei@163.com>
2014-02-18 00:20:33 +0800

    add drag drop grid view control

--- iair/src/com/txmcu/activity/CityManageActivity.java
+++ iair/src/com/txmcu/activity/CityManageActivity.java
@@ -2,14 +2,32 @@ package com.txmcu.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.txmcu.iair.R;
 
-public class CityManageActivity extends Activity {
+public class CityManageActivity extends Activity 
+implements OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.city_manage_activity);
+		findViewById(R.id.back_img).setOnClickListener(this);
 	}
+	
+	@Override
+	public void onClick(View paramView)
+	{
+	    switch (paramView.getId())
+	    {
+	    	case R.id.back_img:
+	    		finish();
+	    		break;
+	    	
+	    }
+	}
+	
+	
 }
