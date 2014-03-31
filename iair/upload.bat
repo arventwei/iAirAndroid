589d63292c099921c8611708806a691bb69c631f
arventwei <arventwei@163.com>
2014-04-01 01:19:23 +0800

    build 1.0.3

--- iair/upload.bat
+++ iair/upload.bat
@@ -1,1 +1,1 @@
-curl -F "apiKey=qk3ne7ortm8emgdf" -F "apk=@iAir-1.0.2.apk" https://push-link.com/apps/api_upload
+curl -F "apiKey=qk3ne7ortm8emgdf" -F "apk=@iAir-1.0.3.apk" https://push-link.com/apps/api_upload
